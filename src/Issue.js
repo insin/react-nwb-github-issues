1bd6cef265283a1bc2eba31d22fc9680f045b07b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-12-06 09:56:32 +0000

    Add comments

--- src/Issue.js
+++ src/Issue.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Link} from 'react-router'
 import TimeAgo from 'react-timeago'
 
+import IssueComment from './IssueComment'
+
 let fetchIssue = ({username, repo_name, issue_number}, cb) => {
   window.fetch(`https://api.github.com/repos/${username}/${repo_name}/issues/${issue_number}`)
     .then(res => res.json())
@@ -9,7 +11,7 @@ let fetchIssue = ({username, repo_name, issue_number}, cb) => {
       window.fetch(`https://api.github.com/repos/${username}/${repo_name}/issues/${issue_number}/comments`)
         .then(res => res.json())
         .then(comments => {
-          issue.comments
+          issue.comments = comments
           cb(null, {issue})
         })
     })
@@ -34,21 +36,10 @@ export default React.createClass({
       </div>
 
       <div className="issue-comments">
-        <div>
-          <Link to={`/${issue.user.login}`}>
-            <img className="avatar" height="48" width="48" src={`${issue.user.avatar_url}&s=96`}/>
-          </Link>
-
-          <div className="comment">
-            <p className="comment-header">
-              <span className="user"><Link to={`/${issue.user.login}`}>{issue.user.login}</Link></span>
-              commented <TimeAgo date={issue.created_at}/>
-            </p>
-            <div className="comment-body">
-              {issue.body}
-            </div>
-          </div>
-        </div>
+        <IssueComment comment={issue}/>
+        {issue.comments.map(comment =>
+          <IssueComment comment={comment}/>
+        )}
       </div>
     </div>
   }
---
+++ src/IssueComment.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Link} from 'react-router'
+import TimeAgo from 'react-timeago'
+
+export default React.createClass({
+  render() {
+    let {comment} = this.props
+    return <div>
+      <Link to={`/${comment.user.login}`}>
+        <img className="avatar" height="48" width="48" src={`${comment.user.avatar_url}&s=96`}/>
+      </Link>
+      <div className="comment">
+        <p className="comment-header">
+          <span className="user"><Link to={`/${comment.user.login}`}>{comment.user.login}</Link></span>
+          commented <TimeAgo date={comment.created_at}/>
+        </p>
+        <div className="comment-body">
+          {comment.body}
+        </div>
+      </div>
+    </div>
+  }
+})
