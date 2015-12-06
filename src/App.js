cad3abd4ec47f78bf50194ec1bd7cbfb1068e733
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-12-06 05:39:42 +0000

    FontAwesome

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import Icon from 'react-fa'
+
 export default React.createClass({
   render() {
     return <div>
-      <h2>Welcome to React</h2>
+      <header>
+        <a href="https://github.com/insin/nwb" target="_blank">
+          <Icon name="github"/>
+        </a>
+
+        <a href="https://github.com/insin/react-nwb-github-issues" target="_blank" className="btn primary fork">
+          <Icon name="code-fork"/> Fork Me on GitHub
+        </a>
+      </header>
+
+      {this.props.children}
     </div>
   }
 })
