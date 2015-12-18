9dc87818e45b207af8bcb36632d4b344b8dfcd8b
Jonny Buchanan <jonathan.buchanan@gmail.com>
2015-12-18 02:37:03 +0000

    Use the GitHub logo as a loading throbber

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, {PropTypes} from 'react'
 import Icon from 'react-fa'
 
 export default React.createClass({
+  contextTypes: {
+    asyncProps: PropTypes.object
+  },
   render() {
+    let {loading} = this.context.asyncProps
     return <div>
       <header>
         <a href="https://github.com/insin/nwb" target="_blank">
-          <Icon name="github"/>
+          <Icon name="github" className={loading ? 'loading' : ''}/>
         </a>
 
         <a href="https://github.com/insin/react-nwb-github-issues" target="_blank" className="btn primary fork">
